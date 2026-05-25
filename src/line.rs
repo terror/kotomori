9ed8dff794018f5a1effa98f775dc1ffb06f03d1
Liam <liam@scalzulli.com>
2026-05-25 12:53:49 -0400

    Tweak

--- src/line.rs
+++ src/line.rs
@@ -77,15 +77,15 @@ impl Display for Line {
   }
 }
 
-impl From<Vec<Span>> for Line {
-  fn from(spans: Vec<Span>) -> Self {
-    Self { spans }
-  }
-}
-
 impl From<Line> for Vec<Span> {
   fn from(line: Line) -> Self {
     line.spans
+  }
+}
+
+impl From<Vec<Span>> for Line {
+  fn from(spans: Vec<Span>) -> Self {
+    Self { spans }
   }
 }
 
