9c261e49a72a022cf20a0d59c2a21ec4676d55be
liam <liam@scalzulli.com>
2026-05-26 20:52:54 -0400

    Simplify header component render (#30)

--- src/header.rs
+++ src/header.rs
@@ -5,13 +5,10 @@ pub(crate) struct Header;
 
 impl Component for Header {
   fn render(&self, _width: u16) -> Vec<Line> {
-    vec![
-      vec![
-        Span::styled(env!("CARGO_PKG_NAME"), Style::CyanBold),
-        Span::raw("  "),
-        Span::styled(env!("CARGO_PKG_VERSION"), Style::DarkGray),
-      ]
-      .into(),
-    ]
+    vec![Line::from(vec![
+      Span::styled(env!("CARGO_PKG_NAME"), Style::CyanBold),
+      Span::raw("  "),
+      Span::styled(env!("CARGO_PKG_VERSION"), Style::DarkGray),
+    ])]
   }
 }
