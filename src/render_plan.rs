db91610b52f560029860b228597c3bebc4606751
liam <liam@scalzulli.com>
2026-08-06 23:18:51 -0400

    Simplify render plans (#107)

--- src/render_plan.rs
+++ src/render_plan.rs
@@ -1,22 +1,13 @@
 use super::*;
 
 #[derive(Clone, Debug, Eq, PartialEq)]
-pub(crate) enum RenderPlan<'a> {
-  Full {
-    clear: bool,
-  },
-  NoOperation {
-    previous: &'a PresentedFrame,
-    previous_viewport: Viewport,
-  },
-  Patch {
-    previous: &'a PresentedFrame,
-    previous_viewport: Viewport,
-    diff: Diff,
-  },
+pub(crate) enum RenderPlan {
+  Full { clear: bool },
+  NoOperation { viewport: Viewport },
+  Patch { viewport: Viewport, diff: Diff },
 }
 
-impl RenderPlan<'_> {
+impl RenderPlan {
   pub(crate) fn clears(&self) -> bool {
     matches!(self, Self::Full { clear: true })
   }
