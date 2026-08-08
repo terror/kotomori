5322a0794b6b4f5d714f4833ba505ea45faf9f2c
Liam <liam@scalzulli.com>
2026-08-07 21:53:59 -0400

    Add assert_matches test helper

---
+++ src/testing.rs
@@ -0,0 +1,12 @@
+macro_rules! assert_matches {
+  ($expression:expr, $( $pattern:pat_param )|+ $( if $guard:expr )? $(,)?) => {
+    match $expression {
+      $( $pattern )|+ $( if $guard )? => {}
+      left => panic!(
+        "assertion failed: (left ~= right)\n  left: `{:?}`\n right: `{}`",
+        left,
+        stringify!($($pattern)|+ $(if $guard)?)
+      ),
+    }
+  }
+}
